4199735a97b99103c55db4a4da5c84cd7ba550b6
WangZY <76583642+xmx@users.noreply.github.com>
2023-03-22 11:28:55 +0800

    添加更多的错误判断处理

--- problem/handler.go
+++ problem/handler.go
@@ -1,9 +1,16 @@
 package problem
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"net"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/vela-ssoc/backend-common/validate"
 	"github.com/xgfone/ship/v5"
 	"gorm.io/gorm"
 )
@@ -44,9 +51,41 @@ func (h *handle) HandleError(c *ship.Context, e error) {
 	switch err := e.(type) {
 	case ship.HTTPServerError:
 		ret.Status = err.Code
+	case *validate.TranError:
+		ret.Title = "参数校验错误"
 	case *time.ParseError:
-		ret.Title = "参数错误"
-		ret.Detail = "时间格式错误（正确格式：" + err.Layout + "）"
+		ret.Title = "参数格式错误"
+		ret.Detail = "时间格式错误，正确格式：" + err.Layout
+	case *net.ParseError:
+		ret.Title = "参数格式错误"
+		ret.Detail = err.Text + " 不是有效的 " + err.Type
+	case base64.CorruptInputError:
+		ret.Title = "参数格式错误"
+		ret.Detail = "base64 编码错误：" + err.Error()
+	case *json.SyntaxError:
+		ret.Title = "报文格式错误"
+		ret.Detail = "请求报错必须是 JSON 格式"
+	case *json.UnmarshalTypeError:
+		ret.Title = "数据类型错误"
+		ret.Detail = err.Field + " 收到无效的数据类型"
+	case *strconv.NumError:
+		ret.Title = "数据类型错误"
+		var msg string
+		if sn := strings.SplitN(err.Func, "Parse", 2); len(sn) == 2 {
+			msg = err.Num + " 不是 " + strings.ToLower(sn[1]) + " 类型"
+		} else {
+			msg = "类型错误：" + err.Num
+		}
+		ret.Detail = msg
+	case *mysql.MySQLError:
+		switch err.Number {
+		case 1062:
+			ret.Detail = "数据已存在"
+		default:
+			c.Errorf("SQL 执行错误：%v", e)
+			ret.Status = http.StatusInternalServerError
+			ret.Detail = "内部错误"
+		}
 	default:
 		switch {
 		case err == gorm.ErrRecordNotFound:
@@ -54,7 +93,14 @@ func (h *handle) HandleError(c *ship.Context, e error) {
 		}
 	}
 
-	// 按照 RFC7807 规范 Content-Type 设置为 application/problem+json 更为妥当，
-	// 此处偷个懒使用默认 application/json。
-	_ = c.JSON(ret.Status, ret)
+	// 按照 RFC7807 规范 Content-Type 设置为 application/problem+json 更为妥当。
+	// https://www.rfc-editor.org/rfc/rfc7807.html#section-3
+	buf := c.AcquireBuffer()
+	if err := json.NewEncoder(buf).Encode(ret); err == nil {
+		c.SetRespHeader(ship.HeaderContentLanguage, "zh")
+		c.SetContentType("application/problem+json; charset=UTF-8")
+		c.WriteHeader(ret.Status)
+		_, _ = c.Write(buf.Bytes())
+	}
+	c.ReleaseBuffer(buf)
 }
